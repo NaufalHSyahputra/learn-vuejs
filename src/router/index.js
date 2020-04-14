8195b8250f0889bad423b9ad16ff08f1be4496bc
Naufal Hakim Syahputra <naufalhsyahputra@gmail.com>
2020-04-14 14:30:05 +0700

    Real World Vue - Chapter 4

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,30 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventList from '../views/EventList'
+import EventShow from '../views/EventShow'
+import EventCreate from '../views/EventCreate'
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'event-list',
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/event',
+    name: 'event-show',
+    component: EventShow
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate
   }
-];
+]
 
 const router = new VueRouter({
   routes
-});
+})
 
-export default router;
+export default router

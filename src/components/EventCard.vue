7f7f0b273b8d0bd6dc81ae1bf18387ae504ab995
Naufal Syahputra <naufalhsyahputra@gmail.com>
2020-04-14 22:57:13 +0700

    Real World Vue - Chapter 9 (API Calls with Axios)

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: 1 } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
@@ -13,19 +13,8 @@
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Tues Aug 19, 2019',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123', name: 'Test1' },
-          { id: 'def123', name: 'Test2' }
-        ]
-      }
-    }
+  props: {
+    event: Object
   }
 }
 </script>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(res => {
+        this.events = res.data
+      })
+      .catch(err => {
+        console.log(err.response)
+      })
   }
 }
 </script>

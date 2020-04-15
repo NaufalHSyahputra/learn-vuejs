6273fe4b5972b28604656d9520d707b769234663
Naufal Hakim Syahputra <naufalhsyahputra@gmail.com>
2020-04-15 15:51:22 +0700

    Mastering Vuex - Lesson 3 (State & Getters)

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,33 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
   mutations: {},
   actions: {},
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  },
   modules: {}
-});
+})
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an event, {{ user.name }}</h1>
+    <p>This event {{ user.id }}</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style scoped></style>
